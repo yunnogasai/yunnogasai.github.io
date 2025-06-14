d48342d7e22ca653c8830ee27f2b00295c833da2
yunogasai <yunogasai728728@gmail.com>
2025-06-14 15:57:48 +0900

    Some Changes

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,13 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
-export async function GET(context) {
+
+export async function GET(context: any) {
+    const record: Record<string, string> = {
+        "atom": "http://www.w3.org/2005/Atom",
+
+    }
     return rss({
+        xmlns: record,
         title: "Astro Learner | Blog",
         description: "My journey learning Astro",
         site: context.site,
