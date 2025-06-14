6d751b05fd48478c68ff5e42092a21ee3031eaca
yunogasai <yunogasai728728@gmail.com>
2025-06-14 16:06:56 +0900

    rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://yunogasai728.github.io",
+    site: "https://yunnogasai.github.io",
     server: { port: 1116 },
 });
