d97a51c4fce287f8735f66f2e361f0d80460cb8c
yunogasai <yunogasai728728@gmail.com>
2025-06-14 19:42:56 +0900

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import { visualizer } from "rollup-plugin-visualizer";
 // https://astro.build/config
 export default defineConfig({
     site: "https://yunnogasai.github.io",
     server: { port: 1116 },
+    scopedStyleStrategy: "where",
+    build: { format: "file" },
+
+    vite: {
+        plugins: [
+            visualizer({
+                emitFile: true,
+                filename: "stats.html",
+            }),
+        ],
+    },
 });
